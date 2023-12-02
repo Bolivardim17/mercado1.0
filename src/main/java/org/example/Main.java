3a7a21f46d506a4bbcfa423d80da08c751c3d7e4
bolivardim17 <rxnlmdi92@gmail.com>
2023-12-01 21:12:34 -0500

    envio proyecto

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,28 @@
+package org.example;
+
+// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
+// then press Enter. You can now see whitespace characters in your code.
+public class Main {
+
+        //Mercado
+        char clasificacionProducto='a';
+        short precioProducto=11000;
+        int cantidadInventario=35;
+        long codigoDeBarras=1234567890;
+        long numeroDeLote=5357286382L;
+        int unidadDeMedida=1000;
+        float Descuento=0.25F;
+        float impuesto=0.1F;
+        int numeroDeFactura=100307;
+        int identificacionCliente=543;
+        char metodoDePago='f';
+        short puntoDeReorden=5789;
+        byte ubicacionEnElEstante=6;
+        float promocion=0.05F;
+        long ventasDiarias=2500000;
+        char hostorialDeCompras='H';
+        char empleadoDeCaja='f';
+        byte codigoProveedor=127;
+        long egresos=1800000;
+        byte productosCaducados=25;
+}
